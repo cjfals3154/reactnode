f986952ad3311906665e0541198fa4481683bedf
Shin <cjfals3154@naver.com>
2022-05-18 02:51:42 +0900

    gitignore update

--- front/next.config.js
+++ front/next.config.js
@@ -6,7 +6,6 @@ module.exports = withBundleAnalyzer({
   compress: true,
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === "production";
-
     return {
       ...config,
       mode: prod ? "production" : "development",
